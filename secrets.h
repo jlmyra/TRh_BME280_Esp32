e6bb75bada4f9b2e21f899bc4fd8e2413ff88a8d
jlmyra <jlmyra@gmail.com>
2025-11-19 20:52:07 -0500

    Add files via upload

---
+++ secrets.h
@@ -0,0 +1,35 @@
+
+
+
+  #define SECRET_ssid "wifi_name";
+  #define SECRET_password "Wifi_Password";
+
+
+//---------BASEMENT ESP32 TRh-1(myraclegarden.com)------------------
+
+//-------------NODE-MCU ESP32-------------------------
+
+  #define SECRET_myChannelNumber 79378
+  #define SECRET_myWriteAPIKey "TS_Key"
+  #define SECRET_api_key "APT_KEY"
+  #define SECRET_tempCorr 0.984
+  #define SECRET_rhCorr 0.92
+
+  #define SECRET_PROJECT_ID "Google_name"
+  #define SECRET_CLIENT_EMAIL "Google_name.iam.gserviceaccount.com"
+  #define SECRET_spreadsheetId "google spreadsheetneme"
+ 
+  #define SECRET_PRIVATE_KEY PROGMEM "-----BEGIN PRIVATE KEY-----n-----END PRIVATE KEY-----\n";
+
+//---------END BASEMENT ESP32 D1 (myraclegarden.com)-------------
+
+//------------------------------------------------------------
+//    ------QUONSET ESP32 D1 (mgcquonset.com)----------
+//------------------------------------------------------------
+
+//--------------LOLIN D32  TRh-2
+----------------------
+/*
+#
+
+//----------END QUONSET ESP32 (mgcquonset.com)-----------------
